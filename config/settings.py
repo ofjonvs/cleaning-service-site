5871d4ce57a0b7946454de1f75756059f1fc1e4e
Jonas da Silva <jonas.fjds@gmail.com>
2026-02-16 22:58:47 -0500

    add requirements

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0i9&cpksso72n1eiaj-n2tl=c0qu4l$=$um20($#w#j2c^v+&'
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-unsafe-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
