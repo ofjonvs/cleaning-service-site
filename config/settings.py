e86ad918fca21ca7808dffbc8d0394a28411adfe
Jonas da Silva <jonas.fjds@gmail.com>
2026-03-04 19:33:40 -0500

    cloudinary config

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,17 @@ INSTALLED_APPS = [
 
     "home",
     "booking",
+    'cloudinary_storage',
+    'cloudinary',
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
